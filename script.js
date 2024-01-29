f2dd6a19654aed28ae4856ac555245432eeb28bc
username <denesakin@gmail.com>
2024-01-29 23:09:55 +0100

    bugs exist

--- script.js
+++ script.js
@@ -1,59 +1,70 @@
-let openTabs = []
+let openTabs = [];
+let tabObjects = [];
 
 function manageTabs(tabs) {
 
   openTabs = tabs.slice(0);
-
-  //let tab = tabs[Math.floor(Math.random()*tabs.length)];
-
-  //chrome.tabs.highlight({tabs: tab.index})
   
   let list = document.getElementById("list");
 
-  for (let i = 0; i < openTabs.length; ++i) {
-      let div = document.createElement('div');
-      div.classList.add("innerDiv");
-      list.appendChild(div);
-
-      
+  for (let i = 0; i < openTabs.length; i++) {
     
-      let titleP = document.createElement('p');
-      titleP.innerText = openTabs[i].title;
-      div.appendChild(titleP);
+    const tabObj = { 
+      index: i, 
+      url: openTabs[i].url
+    };
 
+    tabObjects.push(tabObj);
 
-      let button = document.createElement('button');
-      let t = document.createTextNode("remove");
-      button.appendChild(t);
+    let div = document.createElement('div');
+    div.classList.add("innerDiv");
+    list.appendChild(div);
+  
+    let titleP = document.createElement('p');
+    titleP.innerText = openTabs[i].title;
+    div.appendChild(titleP);
 
-      div.appendChild(button);
+    let button = document.createElement('button');
+    let t = document.createTextNode("remove");
+    button.appendChild(t);
 
+    div.appendChild(button);
 
+    button.addEventListener("click", async () => {
 
-      button.addEventListener("click", async () => {
+      //tabObjects.splice(tabObjects.find(a => a.index === tabObj.index) , 1);
 
-        let tab = openTabs[Math.floor(Math.random() * openTabs.length)];
-        chrome.tabs.highlight({tabs: tab.index});
+      let indexTest = tabObjects.map(function(obj) {
+        return obj.index
+      }).indexOf(tabObj.index);
+    
+      tabObjects.splice(indexTest, 1);
 
-      });
-
+      //let tab = openTabs[Math.floor(Math.random() * openTabs.length)];
+      //chrome.tabs.highlight({tabs: tab.index});
+    });
   }
-
 }
 
 function onError(error) {
   console.error(`Error: ${error}`);
 }
 
+const selectRandomBtn = document.getElementById("selectRandom");
 
-const button = document.querySelector("button");
+selectRandomBtn.addEventListener("click", async () => {
 
-button.addEventListener("click", async () => {
-  let tab = openTabs[Math.floor(Math.random() * openTabs.length)];
+  let randomObj = tabObjects[Math.floor(Math.random() * tabObjects.length)];
 
-  chrome.tabs.highlight({tabs: tab.index})
-
+  chrome.tabs.highlight({tabs: randomObj.index})
 });
 
+/*
+const getButton = document.getElementById("getTabs");
+
+getButton.addEventListener("click", async () => {
+
+  chrome.tabs.query({ currentWindow: true }).then(manageTabs, onError);
+}); */
 
 chrome.tabs.query({ currentWindow: true }).then(manageTabs, onError);
